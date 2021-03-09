7c31134b941968cd5348d1eb773cf7e066660421
Pau Masip <pau.masip@enti.cat>
2021-03-09 19:19:37 +0100

    Second First Commit

---
+++ MasterMind/MasterMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterMindApp.swift
+//  MasterMind
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MasterMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MasterMind/ViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModel.swift
+//  MasterMind
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
