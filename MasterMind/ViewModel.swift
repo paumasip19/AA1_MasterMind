84301251e1b3b59d26363e7edd998b13a2350cad
Pau Masip <pau.masip@enti.cat>
2021-03-18 12:21:24 +0100

    Indicators Fully Working

--- MasterMind/ViewModel.swift
+++ MasterMind/ViewModel.swift
@@ -73,42 +73,43 @@ class ViewModel: ObservableObject {
                 }
                 else
                 {
+                    var red: [Int] = [0, 0, 0]
+                    var green: [Int] = [0, 0, 0]
+                    var blue: [Int] = [0, 0, 0]
+                    var black: [Int] = [0, 0, 0]
+                    
                     var exact: Int = 0
+                    var actualInd: Int = 0
                     
                     //Exact place and color
                     for(index, _) in realCombination.enumerated() {
                         if(realCombination[index] == actualComb[index])
                         {
                             exact += 1
+                            combinations[actualTry].indicators[actualInd] = .red
+                            actualInd += 1
+                            
+                            switch actualComb[index] {
+                                case .red:
+                                    red[0] += 1
+
+                                case .green:
+                                    green[0] += 1
+
+                                case .blue:
+                                    blue[0] += 1
+                                    
+                                case .black:
+                                    black[0] += 1
+                                    
+                                default:
+                                    print("Error in combination")
+                            }
+                            
                         }
                     }
-                    
                     
                     //Exact color, not place
-        
-                    var red: [Int] = [0, 0]
-                    var green: [Int] = [0, 0]
-                    var blue: [Int] = [0, 0]
-                    var black: [Int] = [0, 0]
-                    
-                    for (index, _) in actualComb.enumerated() {
-                        switch actualComb[index] {
-                            case .red:
-                                red[0] += 1
-
-                            case .green:
-                                green[0] += 1
-
-                            case .blue:
-                                blue[0] += 1
-                                
-                            case .black:
-                                black[0] += 1
-                                
-                            default:
-                                print("Error in combination")
-                        }
-                    }
                     
                     for (index, _) in realCombination.enumerated() {
                         switch realCombination[index] {
@@ -129,37 +130,77 @@ class ViewModel: ObservableObject {
                         }
                     }
                     
-                    var notExact: Int = 0
-                    
-                    if(red[0] == red[1]) { notExact += red[0] }
-                    if(green[0] == green[1]) { notExact += green[0] }
-                    if(blue[0] == blue[1]) { notExact += blue[0] }
-                    if(black[0] == black[1]) { notExact += black[0] }
-                    
-                    notExact -= exact
-                    
-                    var tempExact: Int = exact
-                    
-                    for(index, _) in combinations[actualTry].indicators.enumerated() {
-                        if(tempExact != 0)
-                        {
-                            combinations[actualTry].indicators[index] = .red
-                            tempExact -= 1
-                        }
-                        else
-                        {
-                            if(notExact != 0)
-                            {
-                                combinations[actualTry].indicators[index] = .white
-                                notExact -= 1
-                            }
+                    for (index, _) in actualComb.enumerated() {
+                        switch actualComb[index] {
+                            case .red:
+                                red[2] += 1
+
+                            case .green:
+                                green[2] += 1
+
+                            case .blue:
+                                blue[2] += 1
+                                
+                            case .black:
+                                black[2] += 1
+                                
+                            default:
+                                print("Error in combination")
                         }
                     }
-                                      
+                    
+                    actualInd = checkColor(c: red, actInd: actualInd)
+                    actualInd = checkColor(c: green, actInd: actualInd)
+                    actualInd = checkColor(c: blue, actInd: actualInd)
+                    actualInd = checkColor(c: black, actInd: actualInd)
+ 
                     actualTry += 1
                 }
                 
             }
         }
     }
+    
+    func checkColor(c: [Int], actInd: Int) -> Int
+    {
+        var color: [Int] = c
+        var actIndTemp: Int = actInd
+        
+        var whitePegs: Int = 0
+        
+        if(color[0] != 0)
+        {
+            color[1] -= color[0]
+            color[2] -= color[0]
+        }
+        
+        if(color[2] != 0 && color[1] != 0)
+        {
+            if(color[1] == color[2])
+            {
+                whitePegs += color[1]
+            }
+            else if(color[1] < color[2])
+            {
+                whitePegs += color[1]
+            }
+            else if(color[1] > color[2])
+            {
+                whitePegs += color[2]
+            }
+        }
+        
+        for (index, _) in realCombination.enumerated()
+        {
+            if(index == actIndTemp && whitePegs != 0)
+            {
+                combinations[actualTry].indicators[index] = .white
+                whitePegs -= 1
+                actIndTemp += 1
+            }
+        }
+        
+        return actIndTemp
+    }
+    
 }
